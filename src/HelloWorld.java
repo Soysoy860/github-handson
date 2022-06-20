80d767c33f0fbd46b047c9bd8b30154758086da7
watnb <watnb153@gmail.com>
2022-06-20 19:38:46 +0900

    初プッシュ

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        String greeting = "Hello";
+        String name = "Soy";
+        System.out.println(greeting + name);
+    }
+}
